6d5b55e83a46004ca72ecbef554caaf9af066ab5
HP <HP@DESKTOP-I59OIGT>
2021-11-11 12:47:09 +0530

    user1

---
+++ SDET24_github/src/test/java/github_practice/Demo1.java
@@ -0,0 +1,13 @@
+package github_practice;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+@Test
+		public void demo1()
+		{
+			System.out.println("Hi this is user2 , its my first commit");
+		}
+	}
+
+
---
+++ SDET24_github/src/test/java/github_practice/Demo2.java
@@ -0,0 +1,11 @@
+package github_practice;
+
+import org.testng.annotations.Test;
+
+public class Demo2 {
+ @Test
+	public void demo2()
+	{
+		System.out.println("Hi thi sis user2 , its my first commit");
+	}
+}
